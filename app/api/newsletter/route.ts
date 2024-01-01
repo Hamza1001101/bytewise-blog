fa0888b0857a68ff1783fbe05a4465b00c76976e
Hamza Farah <dado.org@hotmail.com>
2024-01-01 20:29:36 +0100

    add a post about java records

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -35,8 +35,4 @@ export async function POST(request: Request, res) {
   return NextResponse.json(200)
 }
 
-/*
-export async function GET() {
-  return NextResponse.json({ message: 'Hello - GET' });
-}
-*/
+
